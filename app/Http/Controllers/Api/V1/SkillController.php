46a7e1b7340a6dc29c502144918d69f1b4427fd0
Daniel Popescu <same.dan@gmail.com>
2024-09-17 10:01:48 +0200

    Backend CRUD complete

--- app/Http/Controllers/Api/V1/SkillController.php
+++ app/Http/Controllers/Api/V1/SkillController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreSkillRequest;
+use App\Http\Resources\V1\SkillCollection;
 use App\Http\Resources\V1\SkillResource;
 use App\Models\Skill;
 use Illuminate\Http\Request;
@@ -12,7 +13,14 @@ class SkillController extends Controller
 {
     // GET All Skills Index
     public function index() {
-        return response()->json("Skill index");
+        // return response()->json("Skill index");
+        // return SkillResource::collection((Skill::all()));
+        
+        // Return collection
+        // return SkillResource::collection((Skill::paginate(1)));
+
+        // return SkillCollection
+        return new SkillCollection((Skill::paginate(1)));
     }
     // GET specific skill http://127.0.0.1:8000/api/v1/skills/1
     public function show(Skill $skill){
@@ -29,5 +37,11 @@ class SkillController extends Controller
     public function update(StoreSkillRequest $request, Skill $skill) {
         $skill->update($request->validated());
         return response()->json("Skill UPDATEd");
+    }
+
+    // DELETE Skill
+    public function destroy(Skill $skill) {
+        $skill->delete();
+        return response()->json("Skill DELETED");
     }
 }
